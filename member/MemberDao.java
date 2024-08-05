5c6fb22f18c4080cd80d089e5f79afba30d8ed98
ayoung-dev <dkdudab@hanmail.net>
2024-08-05 14:55:05 +0900

    modify updateMember > updatePassword,Name,Location

--- member/MemberDao.java
+++ member/MemberDao.java
@@ -5,7 +5,8 @@ import java.sql.SQLException;
 public interface MemberDao {
     int join(Member member) throws SQLException;
     Member login(String account, String password) throws SQLException;
-    int updateMember(Member member, String category) throws SQLException;
+    int updatePassword(Member member, String newPassword) throws SQLException;
+    int updateName(Member member, String newName) throws SQLException;
+    int updateLocation(Member member, String newLocation) throws SQLException;
     int deleteMember(Member member) throws SQLException;
-
 }
